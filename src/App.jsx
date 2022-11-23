671904694eac2c3c234fa15ddcd7385d1496a703
Damon Marc Rocha II <dmarcr1997@gmail.com>
2022-11-23 22:16:39 +0000

    REC2#2: GIF input logic

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,9 @@ const TEST_GIFS = [
 ]
 const App = () => {
   const [walletAddress, setWalletAddress] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+  const [gifList, setGifList] = useState([]);
+  
   const checkIfWalletIsConnected = async () => {
     if (window?.solana?.isPhantom) {
       console.log('Phantom wallet found!')
@@ -35,6 +38,21 @@ const App = () => {
     }
   };
 
+  const onInputChange = (e) => {
+    const { value } = e.target;
+    setInputValue(value); 
+  }
+
+  const sendGif = async () => {
+    if (inputValue.length > 0) {
+      console.log('Gif link:', inputValue);
+      setGifList([...gifList, inputValue]);
+      setInputValue('');
+    } else {
+      console.log('Empty input. Try again.');
+    }
+  };
+
   const renderNotConnectedContainer = () => (
     <button
       className="cta-button connect-wallet-button"
@@ -46,8 +64,17 @@ const App = () => {
 
   const renderConnectedContainer = () => (
     <div className="connected-container">
+      <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        sendGif();
+      }}
+      >
+        <input type="text" placeholder="Enter img/gif link!" value={inputValue} onChange={onInputChange} />
+        <button type="submit" className="cta-button submit-gif-button">Submit</button>
+      </form>
       <div className="gif-grid">
-        {TEST_GIFS.map(gif => (
+        {gifList.map(gif => (
           <div className="gif-item" key={gif}>
             <img src={gif} alt={gif} />
           </div>
@@ -63,6 +90,18 @@ const App = () => {
     window.addEventListener('load', onLoad);
     return () => window.removeEventListener('load', onLoad);
   }, []);
+
+  useEffect(() => {
+    if (walletAddress) {
+      console.log('Fetching GIF list...');
+      
+      // Call Solana program here.
+  
+      // Set state
+      setGifList(TEST_GIFS);
+    }
+  }, [walletAddress]);
+  
   return (
     <div className="App">
       <div className={walletAddress ? 'authed-container' : 'container'}>
